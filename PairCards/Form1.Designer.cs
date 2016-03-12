38afda47ad740ce4fc139e5af32cbe6f3c75f210
Taylor Rackley <taylorrack@sbcglobal.net>
2016-03-12 11:29:14 -0600

    Finished Project

---
+++ PairCards/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace PairCards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.label_tries = new System.Windows.Forms.Label();
+            this.label_percentage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button8.Location = new System.Drawing.Point(419, 219);
+            this.button8.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 185);
+            this.button8.TabIndex = 19;
+            this.button8.Text = " ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.Location = new System.Drawing.Point(288, 219);
+            this.button7.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 185);
+            this.button7.TabIndex = 18;
+            this.button7.Text = " ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Location = new System.Drawing.Point(157, 218);
+            this.button6.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 185);
+            this.button6.TabIndex = 17;
+            this.button6.Text = " ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Location = new System.Drawing.Point(26, 218);
+            this.button5.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 185);
+            this.button5.TabIndex = 16;
+            this.button5.Text = " ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button16.BackgroundImage")));
+            this.button16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button16.Location = new System.Drawing.Point(419, 600);
+            this.button16.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(125, 185);
+            this.button16.TabIndex = 15;
+            this.button16.Text = " ";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button15.BackgroundImage")));
+            this.button15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button15.Location = new System.Drawing.Point(288, 600);
+            this.button15.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(125, 185);
+            this.button15.TabIndex = 14;
+            this.button15.Text = " ";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button14.BackgroundImage")));
+            this.button14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button14.Location = new System.Drawing.Point(157, 600);
+            this.button14.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(125, 185);
+            this.button14.TabIndex = 13;
+            this.button14.Text = " ";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button13.BackgroundImage")));
+            this.button13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button13.Location = new System.Drawing.Point(26, 600);
+            this.button13.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(125, 185);
+            this.button13.TabIndex = 12;
+            this.button13.Text = " ";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button12.BackgroundImage")));
+            this.button12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button12.Location = new System.Drawing.Point(419, 410);
+            this.button12.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(125, 185);
+            this.button12.TabIndex = 11;
+            this.button12.Text = " ";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
+            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button11.Location = new System.Drawing.Point(288, 410);
+            this.button11.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(125, 185);
+            this.button11.TabIndex = 10;
+            this.button11.Text = " ";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
+            this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button10.Location = new System.Drawing.Point(157, 409);
+            this.button10.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(125, 185);
+            this.button10.TabIndex = 9;
+            this.button10.Text = " ";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button9.Location = new System.Drawing.Point(26, 409);
+            this.button9.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 185);
+            this.button9.TabIndex = 8;
+            this.button9.Text = " ";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Location = new System.Drawing.Point(415, 28);
+            this.button4.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 185);
+            this.button4.TabIndex = 3;
+            this.button4.Text = " ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Location = new System.Drawing.Point(284, 28);
+            this.button3.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 185);
+            this.button3.TabIndex = 2;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::PairCards.Properties.Resources.card_back;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Location = new System.Drawing.Point(153, 27);
+            this.button2.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 185);
+            this.button2.TabIndex = 1;
+            this.button2.Text = " ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::PairCards.Properties.Resources.card_back;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(26, 27);
+            this.button1.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 185);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button17
+            // 
+            this.button17.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button17.Location = new System.Drawing.Point(49, 875);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(123, 42);
+            this.button17.TabIndex = 21;
+            this.button17.Text = "Play Again";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // label_tries
+            // 
+            this.label_tries.AutoSize = true;
+            this.label_tries.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_tries.Location = new System.Drawing.Point(44, 788);
+            this.label_tries.Name = "label_tries";
+            this.label_tries.Size = new System.Drawing.Size(83, 25);
+            this.label_tries.TabIndex = 22;
+            this.label_tries.Text = "Tries : 0";
+            // 
+            // label_percentage
+            // 
+            this.label_percentage.AutoSize = true;
+            this.label_percentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_percentage.Location = new System.Drawing.Point(44, 822);
+            this.label_percentage.Name = "label_percentage";
+            this.label_percentage.Size = new System.Drawing.Size(159, 25);
+            this.label_percentage.TabIndex = 23;
+            this.label_percentage.Text = "Percent correct : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 941);
+            this.Controls.Add(this.label_percentage);
+            this.Controls.Add(this.label_tries);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Memory";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label_tries;
+        private System.Windows.Forms.Label label_percentage;
+    }
+}
+
---
+++ PairCards/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PairCards
+{
+    public partial class Form1 : Form
+    {
+        
+        Timer hideCardsTimer = new Timer();
+
+        int[] cards;
+        List<Button> listOfPairs = new List<Button>();
+        Random rnd;
+        List<Button> listOfCards = new List<Button>();
+
+        int matchingPairs = 0;
+        int userGuess1;
+        int userGuess2;
+        bool userGuessFirst = true;
+        int tries = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            listOfCards.Add(button1);
+            listOfCards.Add(button2);
+            listOfCards.Add(button3);
+            listOfCards.Add(button4);
+            listOfCards.Add(button5);
+            listOfCards.Add(button6);
+            listOfCards.Add(button7);
+            listOfCards.Add(button8);
+            listOfCards.Add(button9);
+            listOfCards.Add(button10);
+            listOfCards.Add(button11);
+            listOfCards.Add(button12);
+            listOfCards.Add(button13);
+            listOfCards.Add(button14);
+            listOfCards.Add(button15);
+            listOfCards.Add(button16);
+            hideCardsTimer.Interval = 1000;
+            hideCardsTimer.Tick += new System.EventHandler(hideCards);
+            resetGame();
+        }
+
+        private void hideCards(object sender, EventArgs e)
+        {
+            listOfCards[userGuess1].BackgroundImage = Properties.Resources.card_back;
+            listOfCards[userGuess2].BackgroundImage = Properties.Resources.card_back;
+            CardsEnabled(true);
+            disablePairs();
+            hideCardsTimer.Enabled = false;
+        }
+
+        void resetGame() {
+
+            label_percentage.Text = "Percent correct : ";
+            button17.Enabled = false;
+            tries = 0;
+            matchingPairs = 0;
+
+            for (int x = 0; x < 16; x++)
+            {
+                listOfCards[x].Enabled = true;
+                listOfCards[x].BackgroundImage = Properties.Resources.card_back;
+                listOfCards[x].BackgroundImageLayout = ImageLayout.Stretch;
+            }
+          
+            cards = new [] {0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7};
+
+            cards = ShuffleArray(cards);
+
+            for (int x = 0; x < 16; x++)
+            {
+                Console.WriteLine(x + " : " + cards[x]);
+            }
+        }
+
+        void gameWon() {
+            percentage();
+            button17.Enabled = true;
+        }
+
+        void percentage() {
+            int percent = (int)Math.Round((double)(100 * 8) / tries);
+            label_percentage.Text = "Percent Correct : " + percent + "%";
+        }
+
+        int[] ShuffleArray(int[] array)
+        {
+            rnd = new Random();
+            for (int i = array.Length; i > 0; i--)
+            {
+                int j = rnd.Next(i);
+                int k = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = k;
+            }
+            return array;
+        }
+
+        void checkUserGuesses(int guess) {
+
+            cardFlip(cards[guess], listOfCards[guess]);
+            listOfCards[guess].Enabled = false;
+
+            if (userGuessFirst)
+            {
+                userGuess1 = guess;
+                userGuessFirst = !userGuessFirst;
+            }
+            else {
+                userGuess2 = guess;
+                // If pair is a match
+                if (cards[userGuess1] == cards[userGuess2])
+                {
+                    matchingPairs++;
+                    listOfCards[userGuess1].Enabled = false;
+                    listOfCards[userGuess2].Enabled = false;
+                    listOfPairs.Add(listOfCards[userGuess1]);
+                    listOfPairs.Add(listOfCards[userGuess2]);
+                    if (matchingPairs >= 8) {
+                        gameWon();
+                    }
+                }
+                // If pair is not a match
+                else {
+                    CardsEnabled(false);
+                    hideCardsTimer.Enabled = true;                    
+                }
+                userGuessFirst = !userGuessFirst;
+                tries++;
+                label_tries.Text = "Tries : " + tries;
+            }
+        }
+
+        void CardsEnabled(bool value) {
+            for(int x = 0; x < listOfCards.Count; x++) {
+                listOfCards[x].Enabled = value;
+            }
+        }
+
+        void disablePairs() {
+            for (int x = 0; x < listOfPairs.Count; x++){
+                listOfPairs[x].Enabled = false;
+            }
+        }
+
+
+
+        void cardFlip(int cardValue, Button card) {
+            switch (cardValue) {
+                case 0:
+                    card.BackgroundImage = Properties.Resources.ace_of_clubs;
+                    break;
+                case 1:
+                    card.BackgroundImage = Properties.Resources.king_of_diamonds;
+                    break;
+                case 2:
+                    card.BackgroundImage = Properties.Resources.queen_of_spades;
+                    break;
+                case 3:
+                    card.BackgroundImage = Properties.Resources.jack_of_hearts;
+                    break;
+                case 4:
+                    card.BackgroundImage = Properties.Resources._2_of_clubs;
+                    break;
+                case 5:
+                    card.BackgroundImage = Properties.Resources._6_of_diamonds;
+                    break;
+                case 6:
+                    card.BackgroundImage = Properties.Resources._10_of_hearts;
+                    break;
+                case 7:
+                    card.BackgroundImage = Properties.Resources._7_of_spades;
+                    break;
+                default:               
+                    break;
+            }
+            card.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(3);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(4);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(5);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(6);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(7);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(8);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(9);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(10);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(11);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(12);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(13);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(14);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            checkUserGuesses(15);
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            resetGame();
+        }
+
+    }
+}
